bd05ea759552ed1de3d05c77768e52107dc727b8
adasfkdghkjhdkjghdskgk <44849612+adasfkdghkjhdkjghdskgk@users.noreply.github.com>
2018-11-17 23:14:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -893,36 +893,7 @@ if (message.content.startsWith('$clear')) { //xRGRx .. By Julian
 
 
 
-client.on('message', message => {
-    if (message.content.startsWith("رابط")) {
 
-  message.channel.createInvite({
-        thing: true,
-        maxUses: 100,
-        maxAge: 86400
-    }).then(invite =>
-      message.author.sendMessage(invite.url)
-    )
-    const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setDescription("| :white_check_mark:  | :heart:  تم ارسال الرابط على الخاص  ")
-      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
-              const Embed11 = new Discord.RichEmbed()
-        .setColor("RANDOM")
-                .setAuthor(message.guild.name, message.guild.iconURL)
-        .setDescription(`
-**
----------------------
--[${message.guild.name}]  هذا هو رابط سيرفر
----------------------
--هذا الرابط صالح ل 100 مستخدم فقط
----------------------
--هذا الرابط صالح لمده 24 ساعه فقط
----------------------
-**`)
-      message.author.sendEmbed(Embed11)
-    }
-});
 
   
   
@@ -1170,7 +1141,7 @@ client.on('message', msg => {
 }
 })
 client.on('ready', () => { //code bot not leave room voice //Bot Is Online
-    client.channels.get("513420542036934657").join(); //by :n3k4a 
+    client.channels.get("513129769991143444").join(); //by :n3k4a 
     });
  
  
